7ab8099abc4b76767a8181e9f22740583cee5dbd
linasc3-ai <67918533+linasc3-ai@users.noreply.github.com>
2023-02-17 11:24:58 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -34,8 +34,8 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300/14) + ' stone';
-    const temperature =  Math.round((94-32)(5/9)) + ' centigrade';
+    const weight = Math.round(300/14) + " stone";
+    const temperature =  Math.round((94-32)(5/9)) + " centigrade";
     newStory = newStory.replace('300 pounds', weight);
     newStory = newStory.replace('94 farenheit', temperature);
   }
